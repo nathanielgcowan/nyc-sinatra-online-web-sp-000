ae8b74e03b1795a393fe9f24f30c184dbe45eba3
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-11-19 20:27:42 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201119202248) do
+ActiveRecord::Schema.define(version: 20201119202701) do
 
   create_table "landmarks", force: :cascade do |t|
     t.string  "name"
     t.integer "year_completed"
     t.integer "figure_id"
+  end
+
+  create_table "titles", force: :cascade do |t|
+    t.string "name"
   end
 
 end
