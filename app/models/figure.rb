504e19062f5ba40f286b834f030f1ad65b73540a
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-11-19 20:35:57 +0000

    Automatically backed up by Learn

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -2,5 +2,5 @@ class Figure < ActiveRecord::Base
   # add relationships here
   has_many :landmarks
   has_many :figure_titles
-  has_many :titles, through :figure_titles
+  has_many :titles, through: :figure_titles
 end
